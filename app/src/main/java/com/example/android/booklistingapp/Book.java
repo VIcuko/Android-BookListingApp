48c9cf051433b0cabd84169f6e4f2b735a1572a8
Vicuko <victorc.fernandez@gmail.com>
2017-08-07 12:36:17 +0200

    Create Book class to store each books information

---
+++ app/src/main/java/com/example/android/booklistingapp/Book.java
@@ -0,0 +1,57 @@
+package com.example.android.booklistingapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Vicuko on 7/8/17.
+ */
+
+public class Book {
+    String mTitle;
+    ArrayList<String> mAuthors;
+    String mPublisher;
+    String mDescription;
+    String mPublishedDate;
+    String mIsbn;
+    String mThumbnailURL;
+
+    public Book(String title, ArrayList<String> authors, String publisher, String description,
+                String publishedDate, String isbn, String thumbnailUrl){
+
+        mTitle = title;
+        mAuthors = authors;
+        mPublisher = publisher;
+        mDescription = description;
+        mPublishedDate = publishedDate;
+        mIsbn = isbn;
+        mThumbnailURL = thumbnailUrl;
+    }
+
+    public String getTitle(){
+      return mTitle;
+    }
+
+    public ArrayList<String> getAuthors(){
+        return mAuthors;
+    }
+
+    public String getPublisher(){
+        return mPublisher;
+    }
+
+    public String getDescription(){
+        return mDescription;
+    }
+
+    public String getPublishedDate(){
+        return mPublishedDate;
+    }
+
+    public String getIsbn(){
+        return mIsbn;
+    }
+
+    public String getThumbnailURL(){
+        return mThumbnailURL;
+    }
+}
