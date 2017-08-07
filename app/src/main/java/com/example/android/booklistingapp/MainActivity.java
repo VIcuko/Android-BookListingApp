cca1638fdc4e41a88b0f821d5fd6b342bbceedad
Vicuko <victorc.fernandez@gmail.com>
2017-08-07 18:20:01 +0200

    Modify main activity to assing adapter to listview and consider no internet connection case

--- app/src/main/java/com/example/android/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -1,15 +1,55 @@
 package com.example.android.booklistingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private TextView mEmptyView;
+    private ImageView mSearchButton;
+    private TextInputEditText mSearchText;
+//    private BookAdapter mAdapter;
+    private static final String GOOGLE_BOOKS_REQUEST_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+    public static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mEmptyView = (TextView) findViewById(R.id.empty_view);
 
+        if (isOnline()) {
+
+            ListView listView = (ListView) findViewById(R.id.list);
+
+            listView.setEmptyView(mEmptyView);
+
+//            mAdapter = new BookAdapter(this, new ArrayList<Book>());
+
+//            listView.setAdapter(mAdapter);
+        }
+        else{
+            mProgressBar.setVisibility(View.GONE);
+            mEmptyView.setText(R.string.no_connection);
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
     }
 }
