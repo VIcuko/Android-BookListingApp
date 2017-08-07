f756ff98ed2cdda0c35c1b626072b229ae314994
Vicuko <victorc.fernandez@gmail.com>
2017-08-07 21:16:04 +0200

    Assign groupview information in BookAdapter

--- app/src/main/java/com/example/android/booklistingapp/BookAdapter.java
+++ app/src/main/java/com/example/android/booklistingapp/BookAdapter.java
@@ -1,12 +1,11 @@
 package com.example.android.booklistingapp;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,23 +13,100 @@ import java.util.ArrayList;
  * Created by Vicuko on 7/8/17.
  */
 
-public class BookAdapter extends ArrayAdapter<Book> {
+public class BookAdapter extends BaseExpandableListAdapter {
 
-    public BookAdapter(@NonNull Context context, ArrayList<Book> booksArray) {
-        super(context, 0, booksArray);
+    private Context mContext;
+    private ArrayList<Book> mbooks;
+
+
+    public BookAdapter(Context context, ArrayList<Book> booksArrayList) {
+        mContext = context;
+        mbooks = booksArrayList;
     }
 
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View listView = convertView;
-        if (listView == null) {
-            listView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+    public int getGroupCount() {
+        return mbooks.size();
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return mbooks.get(i);
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View groupView, ViewGroup viewGroup) {
+        if (groupView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            groupView = infalInflater.inflate(R.layout.list_group, null);
         }
 
-        Book book = getItem(position);
+        TextView title = (TextView) groupView.findViewById(R.id.title);
+        title.setText(mbooks.get(i).getTitle());
+
+        TextView author = (TextView) groupView.findViewById(R.id.author);
+        author.setText(parseAuthors(mbooks.get(i).getAuthors()));
+
+        TextView year = (TextView) groupView.findViewById(R.id.published_year);
+        year.setText(mbooks.get(i).getPublishedDate().substring(0,3));
+
+        return groupView;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return mbooks.size();
+    }
 
 
-        return listView;
+    @Override
+    public Object getChild(int i, int i1) {
+        return mbooks.get(i);
+    }
+
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+
+    private String parseAuthors(ArrayList<String> authors) {
+        String authorsTogether="";
+        if (authors != null || authors.get(0) != null) {
+            for (int i = 0; i < authors.size(); i++) {
+                authorsTogether.concat(authors.get(i).toString());
+                String separator;
+                if (i==authors.size()-2){
+                    separator = " and ";
+                }
+                else {
+                    separator = (i == (authors.size() - 1)) ? (".") : (", ");
+                }
+                authorsTogether.concat(separator);
+            }
+        }
+        return authorsTogether;
     }
 }
