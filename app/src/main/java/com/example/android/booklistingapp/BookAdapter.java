f2aeeb6634560e80fa9041c8d27609474894184d
Vicuko <victorc.fernandez@gmail.com>
2017-08-07 18:35:29 +0200

    Update BookAdapter

--- app/src/main/java/com/example/android/booklistingapp/BookAdapter.java
+++ app/src/main/java/com/example/android/booklistingapp/BookAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.booklistingapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -22,6 +23,14 @@ public class BookAdapter extends ArrayAdapter<Book> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listView = convertView;
+        if (listView == null) {
+            listView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Book book = getItem(position);
+
+
+        return listView;
     }
 }
