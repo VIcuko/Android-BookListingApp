515f80727a7000ac37489cb05a8aeba06147cfe4
Vicuko <victorc.fernandez@gmail.com>
2017-08-09 14:28:47 +0200

    Add all app content

---
+++ app/src/main/java/com/example/android/booklistingapp/BookAdapter.java
@@ -0,0 +1,160 @@
+package com.example.android.booklistingapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.example.android.booklistingapp.R.id.description_text;
+import static com.example.android.booklistingapp.R.id.isbn_code;
+
+/**
+ * Created by Vicuko on 7/8/17.
+ */
+
+public class BookAdapter extends BaseExpandableListAdapter {
+
+    private Context mContext;
+    private ArrayList<Book> mbooks;
+    private Bitmap bmp;
+
+
+    public BookAdapter(Context context, ArrayList<Book> booksArrayList) {
+        mContext = context;
+        mbooks = booksArrayList;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return mbooks.size();
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return mbooks.get(i);
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View convertView, ViewGroup viewGroup) {
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_group, null);
+        }
+
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        title.setText(mbooks.get(i).getTitle());
+
+        TextView author = (TextView) convertView.findViewById(R.id.author);
+        author.setText(parseAuthors(mbooks.get(i).getAuthors()));
+
+        TextView year = (TextView) convertView.findViewById(R.id.published_year);
+        if (mbooks.get(i).getPublishedDate() != null && !mbooks.get(i).getPublishedDate().isEmpty()) {
+            year.setText(mbooks.get(i).getPublishedDate().substring(0, 4));
+        }
+
+        final ImageView bookThumbnail = (ImageView) convertView.findViewById(R.id.book_image);
+        final String bookUrl = mbooks.get(i).getThumbnailURL();
+
+
+//        new AsyncTask<Void, Void, Void>() {
+//            @Override
+//            protected Void doInBackground(Void... params) {
+//                try {
+//                    InputStream in = new URL(bookUrl).openStream();
+//                    bmp = BitmapFactory.decodeStream(in);
+//                } catch (Exception e) {
+//                    // log error
+//                }
+//                return null;
+//            }
+//
+//            @Override
+//            protected void onPostExecute(Void result) {
+//                if (bmp != null)
+//                    bookThumbnail.setImageBitmap(bmp);
+//            }
+//
+//        }.execute();
+
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return mbooks.size();
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return mbooks.get(i);
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View convertView, ViewGroup viewGroup) {
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_item, null);
+        }
+
+        TextView descriptionLabel = (TextView) convertView.findViewById(R.id.descripton_label);
+        descriptionLabel.setText(R.string.description_label);
+
+        TextView descriptionText = (TextView) convertView.findViewById(description_text);
+        descriptionText.setText(mbooks.get(i).getDescription());
+
+        TextView isbnLabel = (TextView) convertView.findViewById(R.id.isbn_label);
+        isbnLabel.setText(R.string.isbn_label);
+
+        TextView isbnCode = (TextView) convertView.findViewById(isbn_code);
+        isbnCode.setText(mbooks.get(i).getIsbn());
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+
+    private String parseAuthors(ArrayList<String> authors) {
+        String authorsTogether="";
+        if (authors != null) {
+            for (int i = 0; i < authors.size(); i++) {
+                authorsTogether = authorsTogether.concat(authors.get(i).toString());
+                String separator;
+                if (i==authors.size()-2){
+                    separator = " and ";
+                }
+                else {
+                    separator = (i == (authors.size() - 1)) ? ("") : (", ");
+                }
+                authorsTogether = authorsTogether.concat(separator);
+            }
+        }
+        return authorsTogether;
+    }
+}
---
+++ app/src/main/java/com/example/android/booklistingapp/QueryUtils.java
@@ -0,0 +1,172 @@
+package com.example.android.booklistingapp;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.android.booklistingapp.MainActivity.LOG_TAG;
+
+/**
+ * Created by Vicuko on 8/8/17.
+ */
+
+public final class QueryUtils {
+
+    private QueryUtils() {
+    }
+
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the book JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    public static ArrayList<Book> extractBooks(String bookJSON) {
+
+        if (TextUtils.isEmpty(bookJSON)) {
+            return null;
+        }
+
+        ArrayList<Book> books = new ArrayList<>();
+
+        try {
+            JSONObject jsonObj = new JSONObject(bookJSON);
+
+            JSONArray features = jsonObj.getJSONArray("items");
+
+            for (int i = 0; i < features.length(); i++) {
+                JSONObject bookInfo = features.getJSONObject(i);
+                JSONObject properties = bookInfo.getJSONObject("volumeInfo");
+
+                String title = properties.has("title") ? properties.getString("title") : "";
+
+                ArrayList<String> authors = new ArrayList<String>();
+                JSONArray jArray = properties.has("authors") ? properties.getJSONArray("authors") : null;
+                if (jArray != null) {
+                    for (int j = 0; j < jArray.length(); j++) {
+                        authors.add(jArray.getString(j));
+                    }
+                }
+
+                String publishedDate = properties.has("publishedDate") ? properties.getString("publishedDate") : "";
+
+                String description = properties.has("description") ? properties.getString("description") : "";
+
+                JSONObject imageLinks = properties.has("imageLinks") ? properties.getJSONObject("imageLinks") : null;
+
+                String url = "";
+                if (imageLinks != null) {
+                    url = imageLinks.has("smallThumbnail") ? imageLinks.getString("smallThumbnail") : "";
+                }
+
+                JSONArray industryIdentifiers = properties.has("industryIdentifiers") ? properties.getJSONArray("industryIdentifiers") : null;
+
+                String isbn = "";
+                if (industryIdentifiers != null) {
+
+                    for (int k = 0; k < industryIdentifiers.length(); k++) {
+                        JSONObject isbnObject = industryIdentifiers.getJSONObject(k);
+                        String isbnType = isbnObject.getString("type");
+                        if (industryIdentifiers.length() == 1) {
+                            isbn = isbnObject.getString("identifier");
+                        } else {
+                            if (Integer.parseInt(isbnType.substring(5)) == 13) {
+                                isbn = isbnObject.has("identifier") ? isbnObject.getString("identifier") : "";
+                            }
+                        }
+                    }
+                }
+
+                Book book = new Book(title, authors, description, publishedDate, isbn, url);
+                books.add(book);
+            }
+
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem parsing the book JSON results", e);
+        }
+
+        return books;
+    }
+
+    public static List<Book> fetchBooksData(String requestUrl) {
+        URL url = createUrl(requestUrl);
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+        List<Book> books = extractBooks(jsonResponse);
+
+        return books;
+    }
+
+}
